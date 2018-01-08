8daa16e5f9985fa2a767ea98d0a9b4763c1e99b4
Krzysztof Kocel <kkocel20@gmail.com>
2018-01-08 23:17:06 +0100

    Get rid of profiles

--- src/test/kotlin/com/example/safetynet/NonceTestProvider.kt
+++ src/test/kotlin/com/example/safetynet/NonceTestProvider.kt
@@ -3,11 +3,9 @@ package com.example.safetynet
 import com.example.safetynet.cache.NonceCache
 import com.example.safetynet.nonce.NonceProvider
 import com.example.safetynet.nonce.UserIdentifier
-import org.springframework.context.annotation.Profile
 import org.springframework.stereotype.Component
 
 @Component
-@Profile("test")
 internal class NonceTestProvider(nonceCache: NonceCache) : NonceProvider(nonceCache) {
 
     fun putNonceWithIdentifier(nonce: String, userIdentifier: UserIdentifier) {
--- src/test/kotlin/com/example/safetynet/TimeTestProvider.kt
+++ src/test/kotlin/com/example/safetynet/TimeTestProvider.kt
@@ -2,11 +2,9 @@ package com.example.safetynet
 
 import com.example.safetynet.login.TimeProvider
 import org.springframework.context.annotation.Primary
-import org.springframework.context.annotation.Profile
 import org.springframework.stereotype.Component
 
 @Component
-@Profile("test")
 @Primary
 internal class TimeTestProvider : TimeProvider() {
     var fakeCurrentTimestampMinusOneHOur: Long = 0
