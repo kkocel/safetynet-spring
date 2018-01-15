c0dc4dc100ada1419bd5af6d713d18567d2477f3
Krzysztof Kocel <kkocel20@gmail.com>
2018-01-15 16:22:09 +0100

    Ignoring apkDigestSha256

--- src/main/kotlin/com/example/safetynet/login/AttestationStatement.kt
+++ src/main/kotlin/com/example/safetynet/login/AttestationStatement.kt
@@ -9,7 +9,9 @@ data class AttestationStatement(
         @field:Key var timestampMs: Long = 0,
         @field:Key var apkPackageName: String = "",
         @field:Key var apkCertificateDigestSha256: Array<String> = emptyArray(),
-        @field:Key var apkDigestSha256: String = "",
+        /* @field:Key var apkDigestSha256: String = "", */
+        // NOTICE: Checking and managing every apk digest can be
+        // cumbersome and I decided to skip checking it.
         @field:Key var ctsProfileMatch: Boolean = false,
         @field:Key var basicIntegrity: Boolean = false
 ) : JsonWebToken.Payload()
